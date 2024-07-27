8847733fbfa831ed2b6cfa3c3321ae6a3b36c93e
Jack Jury <jackesjury@gmail.com>
2024-07-27 14:55:18 +0100

    made dates work

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,19 +4,6 @@ import { supabase } from "./auth/supabaseClient";
 
 import DatesList from "./DatesList";
 
-const sampleData = [
-  {
-    created_at: "2024-07-27T08:39:48.380147+00:00",
-    date: "2024-07-27",
-    event_desc: null,
-    event_label: "ANNIVERSARY",
-    event_person: null,
-    id: 1,
-    reminder: "2024-07-27T10:39:33+00:00",
-    user_id: null,
-  },
-];
-
 function Dashboard({ session }) {
   const [dates, setdates] = useState([]);
   const getData = async () => {
--- src/components/NewEvent.jsx
+++ src/components/NewEvent.jsx
@@ -3,9 +3,12 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { supabase } from "./auth/supabaseClient";
 
-const NewEvent = () => {
+const NewEvent = ({ session }) => {
   const schema = yup.object().shape({
-    email: yup.string().email().required(),
+    event_person: yup.string().required(),
+    event_label: yup.string().required(),
+    event_desc: yup.string().required(),
+    date: yup.date().required(),
   });
 
   const {
@@ -18,32 +21,45 @@ const NewEvent = () => {
 
   const onSubmit = async (data) => {
     console.log(data);
-    const { data: response, error } = await supabase.auth.signInWithOtp({
-      email: data.email,
-      options: {
-        emailRedirectTo: "/dashboard",
-      },
-    });
+    data.user_id = session.user.id;
+    const { data: response, error } = await supabase
+      .from("dates")
+      .insert([data])
+      .select();
     console.log(error, response);
   };
 
   return (
     <>
       <h2>Log an event</h2>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <fieldset>
           <input
-            name="recipient"
+            name="event_person"
             placeholder="Recipient"
             autoComplete="event"
+            {...register("event_person")}
           />
-          <input name="event" placeholder="Event" autoComplete="event" />
+          <p>{errors.event_person?.message}</p>
+
           <input
-            name="description"
+            name="event_label"
+            placeholder="Event"
+            autoComplete="event"
+            {...register("event_label")}
+          />
+          <p>{errors.event_label?.message}</p>
+          <input
+            name="event_desc"
             placeholder="Description"
             autoComplete="Description"
+            {...register("event_desc")}
           />
-          <input type="date" />
+          <p>{errors.event_desc?.message}</p>
+
+          <input type="date" {...register("date")} />
+          <p>{errors.date?.message}</p>
+
           <input type="submit" value="Add Event" />
         </fieldset>
       </form>
