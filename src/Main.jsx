72657e767a8ff9939768fb4a36f7ad39f62a5bf3
Jack Jury <jackesjury@gmail.com>
2024-07-27 11:32:56 +0100

    pushing to main!

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,14 +1,10 @@
-import React from 'react'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import ReactDOM from 'react-dom/client'
-import Homepage from './components/Homepage.jsx'
-import Login from './components/Login.jsx'
-import SignUp from './components/SignUp.jsx'
-import Dashboard from './components/Dashboard.jsx'
-import './css/index.css'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./components/Homepage.jsx";
+import Login from "./components/Login.jsx";
+import SignUp from "./components/SignUp.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+import "./css/index.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
