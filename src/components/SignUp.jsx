0762e0f4908ebe83b579c2f5ea1d4349f596d2c9
sarahelisabethcominotti <138233386+sarahelisabethcominotti@users.noreply.github.com>
2024-07-27 11:43:09 +0100

    addition of validation

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,25 +1,41 @@
 import "@picocss/pico";
+import { useForm } from "react-hook-form"
+import * as yup from "yup"
+import {yupResolver} from "@hookform/resolvers/yup"
 
 const SignUp = () => {
-    const submit = (data) => {
+
+    const schema = yup.object().shape({
+        name: yup.string().required("Your full name is required"),
+        email: yup.string().email().required(),
+        // password: yup.string().min(4).max(20).required(),
+        // confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Passwords don't match")
+
+    })
+
+    const {register, handleSubmit, formState: {errors}} = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    const onSubmit = (data) => {
         console.log(data)
     }
   return (
     <>
       <h2>Sign Up Page</h2>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <fieldset>
           <label>
             Name
-            <input type="text" name="name" placeholder="Your Name..." />
+            <input type="text" name="name" placeholder="Your Name..." {...register("name")} /><p>{errors.name?.message}</p>
           </label>
           <label>
             Email
-            <input type="email" name="email" placeholder="Your Email..." />
+            <input type="email" name="email" placeholder="Your Email..." {...register("email")}/><p>{errors.email?.message}</p>
           </label>
         </fieldset>
 
-        <input type="submit" value="Sign Up" />
+        <input type="submit" value="Sign Up"/>
       </form>
     </>
   );
